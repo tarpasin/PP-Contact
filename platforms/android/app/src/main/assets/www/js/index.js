2ca208cb31377ac693c66caa572ff25283d4776b
Pasin K <pasin.ke598@cbs.chula.ac.th>
2018-01-25 13:51:53 +0700

    add template

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -28,6 +28,7 @@ var app = {
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
+        window.location = 'login.html';
     },
 
     // Update DOM on a Received Event
